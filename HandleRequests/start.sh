44755336fa8ce0bc4c61dc021f15a6e959d7c541
Ahmad Nadar <ahmadndar@gmail.com>
2018-12-23 12:15:07 +0100

    m

--- HandleRequests/start.sh
+++ HandleRequests/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pwd
+mv model.pkl api/
 ls
 
 python manage.py runserver 0.0.0.0:8000
--- MainGUI/start.sh
+++ MainGUI/start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-
 python manage.py collectstatic --noinput
-
-pwd
-ls
-
 python manage.py runserver 0.0.0.0:7000
